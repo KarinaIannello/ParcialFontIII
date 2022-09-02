1a2eee6d1d5c7b12b463224f6ed40814e35281e7
KarinaIannello <kari.iannello.ki@gmail.com>
2022-09-02 09:58:23 -0300

    first commit

---
+++ src/components/Listado.js
@@ -0,0 +1,21 @@
+// Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
+// El componente Listado es el padre de:
+// - varios Item
+// ESTADO: Listado no necesita manejar un estado.
+// MÉTODOS: Listado no requiere de métodos.
+// PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
+
+import Item from './Item';
+import data from '../data/data.json';
+import styles from '../styles/listado.module.css';
+
+export default function Listado({funcion}) {
+
+  return (
+    <div className={styles.container}>
+      {
+      data.map((producto)=> <Item key = {(producto.id)} producto = {producto} funcionNieto = {funcion}/>)
+      }
+    </div>
+  )
+}
